a37ba25c680321e3643d7a229cf642dc24bfb26c
Mert Can Gürler <109288832+mertcangurler1923@users.noreply.github.com>
2023-05-20 23:33:06 +0300

    Update NetworkTest.java

--- src/main/java/stepDefinitions/NetworkTest.java
+++ src/main/java/stepDefinitions/NetworkTest.java
@@ -19,60 +19,60 @@ public class NetworkTest extends BaseDriver {
         networkPage=new NetworkPage(driver);
         networkPage.networkHomepage();
     }
-    @Given("The network url is checked.")
+    @When("The network url is checked.")
     public void the_network_url_is_checked() {
         List<String> data =networkPage.networkHomepageChecked();
         Assertions.assertEquals("https://www.network.com.tr/",data.get(0));
     }
-    @Given("Search components “jacket” are printed and searched.")
+    @When("Search components “jacket” are printed and searched.")
     public void search_components_jacket_are_printed_and_searched() {
         networkPage.inputJacket();
     }
-    @Given("Scrolling as much as the show more button on the product listing page.")
+    @When("Scrolling as much as the show more button on the product listing page.")
     public void scrolling_as_much_as_the_show_more_button_on_the_product_listing_page() {
         networkPage.showMore();
     }
-    @Given("It is checked that the 2nd page is passed \\(url changes).")
+    @When("It is checked that the 2nd page is passed \\(url changes).")
     public void ıt_is_checked_that_the_2nd_page_is_passed_url_changes() {
         List<String> data=networkPage.twoPageCheck();
         Assertions.assertNotEquals(data.get(0),data.get(1));
     }
-    @Given("You hover over the first discounted product on the product list page, a random size selection is made.")
+    @When("You hover over the first discounted product on the product list page, a random size selection is made.")
     public void you_hover_over_the_first_discounted_product_on_the_product_list_page_a_random_size_selection_is_made() {
         networkPage.discountPercentClick();
     }
-    @Given("Hover the Go to Cart button that opens in the lower right corner.")
+    @When("Hover the Go to Cart button that opens in the lower right corner.")
     public void hover_the_go_to_cart_button_that_opens_in_the_lower_right_corner() {
         networkPage.goToCard();
     }
-    @Given("It is checked that the size and price information of the product is correct in the basket.")
+    @When("It is checked that the size and price information of the product is correct in the basket.")
     public void ıt_is_checked_that_the_size_and_price_information_of_the_product_is_correct_in_the_basket() {
         networkPage.productPrice();
         List<String> data=networkPage.data();
         Assertions.assertEquals(data.get(2),data.get(4));
         Assertions.assertEquals(data.get(3),"size_"+data.get(5));
     }
-    @Given("It is checked that the old price of the product added to the cart is greater than the discounted price.")
+    @When("It is checked that the old price of the product added to the cart is greater than the discounted price.")
     public void ıt_is_checked_that_the_old_price_of_the_product_added_to_the_cart_is_greater_than_the_discounted_price() {
         List<String> data=networkPage.data();
         Double boxDiscountMoneyValue=Double.parseDouble(data.get(2).replace(".","").replace(",",".").replace("TL",""));
         Double boxNotDiscountMoneyValue=Double.parseDouble(data.get(6).replace(".","").replace(",",".").replace("TL",""));
         Assertions.assertTrue(boxDiscountMoneyValue<boxNotDiscountMoneyValue);
     }
-    @Given("Continue button is clicked.")
+    @When("Continue button is clicked.")
     public void continue_button_is_clicked() {
         networkPage.goOn();
     }
-    @Given("Filling in the user information from csv format \\(E-mail - by reading the password from the csv file is necessary.)")
+    @When("Filling in the user information from csv format \\(E-mail - by reading the password from the csv file is necessary.)")
     public void filling_in_the_user_information_from_csv_format_e_mail_by_reading_the_password_from_the_csv_file_is_necessary() throws FileNotFoundException {
         networkPage.idPassword();
     }
-    @Given("Check that the login button has arrived.")
+    @When("Check that the login button has arrived.")
     public void check_that_the_login_button_has_arrived() {
         List<String> data=networkPage.data();
         Assertions.assertEquals(data.get(7),"true");
     }
-    @Given("Click the network logo.")
+    @When("Click the network logo.")
     public void click_the_network_logo() {
         networkPage.networkLogo();
     }
